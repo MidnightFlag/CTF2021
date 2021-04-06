c403e45409bedd19fae170d1f51b5ac163fc77fb
Ubuntu <ubuntu@vps-6f798642.vps.ovh.net>
2021-04-06 16:06:23 +0000

    Delete timing attack challenge.

--- Misc/Secure_Shell/entry.sh
+++ Misc/Secure_Shell/entry.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-	su -c "exec socat TCP-LISTEN:9002,reuseaddr,fork EXEC:'python3 /chall/chall.py',stderr,pty,echo=0" - challenger;
+	su -c "exec socat TCP-LISTEN:9001,reuseaddr,fork EXEC:'python3 /chall/chall.py',stderr,pty,echo=0" - challenger;
 done
--- Misc/Timing_Attack/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while :
-do
-	su -c "exec socat TCP-LISTEN:9001,reuseaddr,fork EXEC:'python3 /chall/chall.py',stderr,pty,echo=0" - challenger;
-done
